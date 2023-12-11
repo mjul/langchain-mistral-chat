d4285c7604a631f80c5256e85e2e933747e991bb
Martin Jul <martin@mjul.com>
2023-12-11 15:01:46 +0100

    Use the Mistral AI 7B instruct model and correct prompt

--- app/chatbot.py
+++ app/chatbot.py
@@ -4,7 +4,9 @@ from langchain.prompts import PromptTemplate
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 from transformers import pipeline
 
-model_id = "mistralai/Mistral-7B-v0.1"
+#model_id = "mistralai/Mistral-7B-v0.1"
+model_id = "mistralai/Mistral-7B-Instruct-v0.1"
+
 # Some smaller models to try for faster iteration if you are on limited hardware:
 # model_id = "bert-large-cased"
 # model_id = "TinyLlama/TinyLlama-1.1B-Chat-v0.6"
@@ -20,10 +22,12 @@ device_map = infer_auto_device_map(
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id, device_map=device_map)
 
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=30)
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=50)
 hf = HuggingFacePipeline(pipeline=pipe)
 
-template = """What is a good name for a company that makes {product}?"""
+template = """[INST]List a good company name and slogan for a business selling {product}?[/INST]"""
+
+
 prompt = PromptTemplate.from_template(template)
 
 chain = prompt | hf
